ca22fade9e6ad00a476cb1773b8eed403140a8cf
Esref Viduslu <esref.viduslu@lab2023.com>
2017-06-13 20:45:18 +0000

    Add routes, index and new actions of exercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "dashboards#index"  
+  root to: "dashboards#index"
+  
+  resources :users do
+    resources :exercises
+  end
 end
